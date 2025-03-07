6ea8cdaa0e5c676a1ac143735add950bcc39e5dd
ctoulouse <camille.toulouse@octo.com>
2025-03-07 11:15:13 +0100

    first commit final

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MainTest {
-  
+    @Test
+    public void shouldTest() {
+        int result = 0;
+        int assertResult = 0;
+        assertEquals(assertResult, result);
+    }
 }
